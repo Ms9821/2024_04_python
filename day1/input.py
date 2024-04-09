2741a5fe2e82e18e1c128ee8d4a5bcd77541106a
Ms9821 <166516048+Ms9821@users.noreply.github.com>
2024-04-09 21:52:02 +0900

    Add files via upload

---
+++ day1/input.py
@@ -0,0 +1,8 @@
+#print: 글 나오도록 하는 기능
+#input: 글을 입력 하는 기능
+
+# drama = input("당신의 최애 드라마는?")
+# print(f"당신의 최애 드라마는 {drama}이군요!")
+#
+# name = input ("당신의 이름은?")
+# print(f"당신의 이름은 {name}이군요
---
+++ day1/print.py
@@ -0,0 +1,3 @@
+print("hello world!")
+print("안녕하세요 저는 조명신 나이는 29살입니다.")
+print("돈이 좋아 스폰지밥~")
