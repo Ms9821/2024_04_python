8b4428ee3d6c6ba51fe57946b9514e153cfdf8bf
Ms9821 <166516048+Ms9821@users.noreply.github.com>
2024-04-11 21:49:42 +0900

    Add files via upload

---
+++ day2/dataType.py
@@ -0,0 +1,28 @@
+# 변수 : RAM에 저장하는 공간
+# 2 + 3 = 5 인간: 연산 + 기억 [뇌]
+# 2 + 3 = 5 컴퓨터: 연산[CPU] + 기억[RAM] (폰노이만 구조)
+
+# 데이터 타입에 따라서 차지하는 RAM공간이 다름
+# a = 2 + 3 [숫자 타입]
+# a = "안녕하세요" [문자 타입]
+#데이터 타입 종류
+
+# print : 출력 기능
+# input : 입력 기능
+# type : 타입을 알려주는 기능
+# print(type("1"))
+# print(type(True))
+# print(type(None))
+# type-casting[타입 변환]
+# 정수화: int()
+d = int("100")
+d1 = int(3.14)
+print(d1)
+
+# 실수화: Float()
+e = float("3.14")
+print(e)
+
+#문자화:str()
+f = str(True)
+print(f)
---
+++ day2/dataType_quiz.py
@@ -0,0 +1,15 @@
+# user = input("첫 번쨰 정수 입력:")
+# user2 = input("두 번째 정수 입력:")
+# result = int(user) + int(user2)
+# print(f"결과: {result}")
+
+# age = input("사용자의 나이가:")
+# year = 2024 - int(age) + 1
+# print (f"나이가 {age}살이라면, 출생년도는 {year}년입니다.")
+
+side = input("정사각형의 한 변의 길이:")
+width = int(side) * int(side)
+print(f"한 변의 길이가 {side}라면, 넓이는 {width}입니다.")
+
+# 불리언화 해주는 기능
+bool() # True or False [0."",None <- 셋은 False [나머진 다 트루]
