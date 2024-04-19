024ff12004ea14c08e0a76b68507981af555c44e
Ms9821 <166516048+Ms9821@users.noreply.github.com>
2024-04-19 17:29:52 +0900

    Add files via upload

---
+++ day03/index.py
@@ -0,0 +1,18 @@
+# 산술, 비교, 대입, 논리
+# 멤버쉽 연산자[in]
+coffee = "americano"
+print('a' in coffee) # True
+print('amri' in coffee) # True
+text = """""" # 두 문장 이상의 글
+news = """먼저, 이마트는 19일부터 25일까지 일주일간 쟁반짜장, 마늘 등갈비찜, 등심 탕수육, 종로식 꼬리곰탕, 피코크 잭슨피자 등 약 20종의 피코크 인기 먹거리를 신세계포인트 적립 시 최대 30% 할인 판매한다고 19일 밝혔다."""
+print("비트코인"in news)
+
+# 슬라이싱 연산자 [:]
+car = "tesla"
+print(car[0:3]) # tes
+print(car[1:3]) # es
+
+# 인덱스 연산자 [[]]
+code = "python"
+print(code[0]) #p
+print(code[3]) #h
