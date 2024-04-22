90de739ba8c0f77e7fff7d088c6deac23fa2bd60
Ms9821 <166516048+Ms9821@users.noreply.github.com>
2024-04-22 22:32:46 +0900

    Add files via upload

---
+++ day04/condition.py
@@ -0,0 +1,46 @@
+#condition_elif.py
+#num = int(input("정수 입력:"))
+
+#if num >= 90:
+ #   print("A등급")
+#elif num >= 80:
+ #   print("B등급")
+#elif num >= 70:
+ #   print("C등급")
+#else:
+ #   print("과락입니다.")
+
+# 국,영,수 점수를 3개 입력 받고,
+# 평균이 90점 이상 'A', 80점 이상 'B',70점 이상 'C',60점 이상 'D'
+# 나머지는 F로 나타내는 프로그램 작성하기
+
+#kor = int(input("국어 점수:"))
+#eng = int(input("영어 점수:"))
+#math = int(input("수학 점수:"))
+#avg = (kor + eng + math) / 3
+
+#if avg >= 90:
+#    print("A")
+#elif avg >= 80:
+ #   print("B")
+#elif avg >= 70:
+ #   print("C")
+#elif avg >= 60:
+ #   print("D")
+#else:
+ #   print("F")
+
+ # nested condition
+ # if문의 depth는 2번까지 지향하는 걸로 !
+
+#  score = int(input("점수 입력:"))
+# if score > 60:
+# if score == 100:
+#      print("만접입니다!")
+# else:
+#      print("합격입니다.")
+# else:
+# if score == 0:
+#     print("이건 좀...")
+# else:
+#     print("불합격입니다.")
---
+++ day04/condition_elif.py
@@ -0,0 +1,11 @@
+#condition_elif
+num = int(input("점수 입력:"))
+
+if num >= 90:
+    print("A등급")
+elif num >= 80:
+    print("B등급")
+elif num >= 70:
+    print("C등급")
+else:
+    print("고")
